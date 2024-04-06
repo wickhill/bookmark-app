021611f78fe08c52ed8b4581fb3c1c3de4ac6db8
dlesesne23 <davidplesesne@gmail.com>
2024-04-06 11:18:25 -0500

    Added Crud routes, still need to test

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,22 @@ app.get("/", (req, res) => {
     res.send("Hello World");
   });
 
+  // Seed route to seed our data
+  app.get('/seed', function (req, res) {
+    // Remove any existing fruits
+    db.Bookmark.deleteMany({})
+        .then(removedBookmarks => {
+            console.log(`Removed ${removedBookmarks.length} bookmarks`)
+
+            // Seed the fruits collection with the seed data
+            db.Fruit.insertMany(db.seedFruits)
+                .then(addedBookmarks => {
+                    console.log(`Added ${addedBookmarks.length} Bookmarks`)
+                    res.json(addedBookmarks)
+                })
+        })
+})
+
 //declare a variable for our port number
 const PORT = process.env.PORT || 3000;
 
